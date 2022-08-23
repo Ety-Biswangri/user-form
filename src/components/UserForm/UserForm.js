39d56ebed59197fdbf0d93b22bc1d2d83ef56693
Ety-Biswangri <etybgc02@gmail.com>
2022-08-23 10:42:21 +0600

    id is saved in the db

--- src/components/UserForm/UserForm.js
+++ src/components/UserForm/UserForm.js
@@ -1,20 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 
 const UserForm = () => {
+
+    const [errormessage, setErrormessage] = useState('');
+
+    const handleUsername = () => {
+
+    }
+
+    const handleForm = (event,) => {
+        event.preventDefault();
+
+        const url = `http://localhost:5000/userinfo`;
+
+        let username = event.target.username.value;
+
+        /* if (username === '') {
+            setErrormessage('Please enter your username');
+            return;
+        } */
+
+        // setErrormessage('');
+
+        const userInfo = {
+            username: username
+        }
+        console.log(userInfo);
+
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(userInfo)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+
+        event.target.reset();
+
+    };
+
     return (
         <div>
             <div className='w-50 mx-auto m-5'>
-                <Form >
+                <Form onSubmit={handleForm}>
                     <Form.Group className="mb-3" controlId="formBasicUsername">
                         <Form.Label>Username</Form.Label>
-                        <Form.Control type="text" placeholder="Enter username" />
+                        <Form.Control type="text" placeholder="Enter username" name='username' onChange={handleUsername} required autoComplete='off' />
                     </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                    </Form.Group>
+
+                    <p className='text-danger'>{errormessage}</p>
 
                     <Button variant="primary" type="submit">
                         Add
